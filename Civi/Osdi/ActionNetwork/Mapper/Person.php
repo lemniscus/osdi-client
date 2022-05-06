3a159b9e93d5826558589e4764f9f046b1e15259
Noah Miller <nm@lemnisc.us>
2022-05-06 11:28:01 -0700

    AN\Mapper\Person: remove unused functions

--- Civi/Osdi/ActionNetwork/Mapper/Person.php
+++ Civi/Osdi/ActionNetwork/Mapper/Person.php
@@ -84,69 +84,6 @@ class Person {
     return $localPerson;
   }
 
-  /**
-   * @return array{select: array, join: array}
-   */
-  public function getFieldsToSelect(): array {
-    return [
-      'select' => [
-        'id',
-        'modified_date',
-        'first_name',
-        'last_name',
-        'is_opt_out',
-        'do_not_email',
-        'do_not_sms',
-        'preferred_language',
-        'email.email',
-        'phone.phone_numeric',
-        'address.street_address',
-        'address.city',
-        'address.state_province_id',
-        'address.postal_code',
-        'address.country_id',
-        'address.country_id:name',
-      ],
-      'join' => [
-        ['Email AS email', 'LEFT', NULL, ['email.is_primary', '=', 1]],
-        ['Phone AS phone', FALSE, NULL, ['phone.phone_type_id:name', '=', '"Mobile"']],
-        ['Address AS address', FALSE, NULL, ['address.is_primary', '=', 1]],
-      ],
-    ];
-  }
-
-  public function getSingleCiviContactById($id): array {
-    $clauses = $this->getFieldsToSelect();
-    $result = Contact::get(FALSE)
-      ->setJoin($clauses['join'])
-      ->setSelect($clauses['select'])
-      ->setWhere([
-        ['id', '=', $id],
-        ['is_deleted', '=', FALSE],
-      ])
-      ->execute();
-    if (!$result->count()) {
-      throw new InvalidArgumentException('Unable to retrieve contact id %d', $id);
-    }
-    $result = $result->single();
-    $result['address.state_province_id:abbreviation'] = NULL;
-    if (isset($result['address.state_province_id'])) {
-      $abbreviation = $this->getAbbreviatedState($result);
-      $result['address.state_province_id:abbreviation'] = $abbreviation;
-    }
-    return $result;
-  }
-
-  private function getAbbreviatedState(array $contactData) {
-    $countryId = $contactData['address.country_id'] ?? \CRM_Core_Config::singleton()->defaultContactCountry;
-    $abbreviationList = \CRM_Core_BAO_Address::buildOptions(
-      'state_province_id',
-      'abbreviate',
-      ['country_id' => $countryId]
-    );
-    return $abbreviationList[$contactData['address.state_province_id']];
-  }
-
   private function getStateAndCountryIdsFromActNetAddress(array $actNetAddress): array {
     $countryId = \CRM_Core_Config::singleton()->defaultContactCountry;
     if (isset($actNetAddress['country'])) {
