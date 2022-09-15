5b03cd3a21759f6a4e589544a394afcff3b325da
Noah Miller <nm@lemnisc.us>
2022-09-15 10:11:21 -0400

    AN\Matcher\TaggingBasic: implement

--- Civi/Osdi/ActionNetwork/Matcher/TaggingBasic.php
+++ Civi/Osdi/ActionNetwork/Matcher/TaggingBasic.php
@@ -2,6 +2,8 @@
 
 namespace Civi\Osdi\ActionNetwork\Matcher;
 
+use Civi\Api4\EntityTag;
+use Civi\Osdi\CrudObjectInterface;
 use Civi\Osdi\LocalRemotePair;
 use Civi\Osdi\Result\MatchResult as MatchResult;
 use Civi\Osdi\SingleSyncerInterface;
@@ -19,11 +21,141 @@ class TaggingBasic extends AbstractMatcher implements \Civi\Osdi\MatcherInterfac
   }
 
   public function tryToFindMatchForLocalObject(LocalRemotePair $taggingPair): MatchResult {
+    $result = new MatchResult(MatchResult::ORIGIN_LOCAL);
+
+    $origin = $taggingPair->getOrigin();
+    $originTagging = $taggingPair->getLocalObject();
+
+    $targetPerson = $this->matchPerson($origin, $originTagging);
+    if (is_null($targetPerson)) {
+      return $result->setStatusCode($result::NO_MATCH);
+    }
+
+    $targetTag = $this->matchTag($origin, $originTagging);
+    if (is_null($targetTag)) {
+      return $result->setStatusCode($result::NO_MATCH);
+    }
+
+    $targetTaggings = $targetPerson->getTaggings();
+    foreach ($targetTaggings as $targetTagging) {
+      if ($targetTagging->getTag()->getId() === $targetTag->getId()) {
+        $result->setMatch($targetTagging);
+        $taggingPair->setTargetObject($targetTagging);
+        $result->setStatusCode($result::FOUND_MATCH);
+        return $result;
+      }
+    }
+
+    $result->setStatusCode($result::NO_MATCH);
+    return $result;
   }
 
-  public function tryToFindMatchForRemoteObject(LocalRemotePair $pair): MatchResult {
-    $result = new MatchResult($pair->getOrigin());
+  public function tryToFindMatchForRemoteObject(LocalRemotePair $taggingPair): MatchResult {
+    $result = new MatchResult(MatchResult::ORIGIN_REMOTE);
+
+    $origin = $taggingPair->getOrigin();
+    $originTagging = $taggingPair->getRemoteObject();
+
+    $targetPerson = $this->matchPerson($origin, $originTagging);
+    if (is_null($targetPerson)) {
+      return $result->setStatusCode($result::NO_MATCH);
+    }
+
+    $targetTag = $this->matchTag($origin, $originTagging);
+    if (is_null($targetTag)) {
+      return $result->setStatusCode($result::NO_MATCH);
+    }
+
+    $civiApiTaggingGet = EntityTag::get(FALSE)
+      ->setWhere([
+          ['entity_table', '=', 'civicrm_contact'],
+          ['entity_id', '=', $targetPerson->getId()],
+          ['tag_id', '=', $targetTag->getId()],
+      ])->execute();
+
+    if (0 == $civiApiTaggingGet->count()) {
+      return $result->setStatusCode($result::NO_MATCH);
+    }
+
+    $targetTagging = call_user_func(
+      [$taggingPair->getLocalClass(), 'fromArray'],
+      $civiApiTaggingGet->first());
+
+    $result->setMatch($targetTagging);
+    $taggingPair->setTargetObject($targetTagging);
+    $result->setStatusCode($result::FOUND_MATCH);
     return $result;
+  }
+
+  public function getPersonSyncer(): ?SingleSyncerInterface {
+    if (empty($this->personSyncer)) {
+      $this->personSyncer = $this->taggingSyncer->getPersonSyncer();
+    }
+    return $this->personSyncer;
+  }
+
+  public function setPersonSyncer(?SingleSyncerInterface $personSyncer): self {
+    $this->personSyncer = $personSyncer;
+    return $this;
+  }
+
+  public function getTagSyncer(): ?SingleSyncerInterface {
+    if (empty($this->tagSyncer)) {
+      $this->tagSyncer = $this->taggingSyncer->getTagSyncer();
+    }
+    return $this->tagSyncer;
+  }
+
+  public function setTagSyncer(?SingleSyncerInterface $tagSyncer): self {
+    $this->tagSyncer = $tagSyncer;
+    return $this;
+  }
+
+  public function getTaggingSyncer(): ?SingleSyncerInterface {
+    return $this->taggingSyncer;
+  }
+
+  public function setTaggingSyncer(?SingleSyncerInterface $taggingSyncer): self {
+    $this->taggingSyncer = $taggingSyncer;
+    return $this;
+  }
+
+  protected function matchPerson(
+    string $origin,
+    CrudObjectInterface $originTagging
+  ): ?CrudObjectInterface {
+    $personSyncer = $this->getPersonSyncer();
+    $personPair = $personSyncer
+      ->toLocalRemotePair()
+      ->setOrigin($origin)
+      ->setOriginObject($originTagging->getPerson());
+    $personMatchResult = $personSyncer->fetchOldOrFindNewMatch($personPair);
+
+    $c = $personMatchResult->getStatusCode();
+    if ($c == $personMatchResult::FOUND_NEW_MATCH
+      || $c == $personMatchResult::FETCHED_SAVED_MATCH) {
+      return $personPair->getTargetObject();
+    }
+    return NULL;
+  }
+
+  protected function matchTag(
+    string $origin,
+    CrudObjectInterface $originTagging
+  ): ?CrudObjectInterface {
+    $tagSyncer = $this->getTagSyncer();
+    $tagPair = $tagSyncer
+      ->toLocalRemotePair()
+      ->setOrigin($origin)
+      ->setOriginObject($originTagging->getTag());
+    $tagMatchResult = $tagSyncer->fetchOldOrFindNewMatch($tagPair);
+
+    $c = $tagMatchResult->getStatusCode();
+    if ($c == $tagMatchResult::FOUND_NEW_MATCH
+      || $c == $tagMatchResult::FETCHED_SAVED_MATCH) {
+      return $tagPair->getTargetObject();
+    }
+    return NULL;
   }
 
 }
---
+++ tests/phpunit/Civi/Osdi/ActionNetwork/Matcher/TaggingBasicTest.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Civi\Osdi\ActionNetwork\Matcher;
+
+use Civi\Osdi\LocalObject\PersonBasic as LocalPerson;
+use Civi\Osdi\MatcherInterface;
+use Civi\Osdi\RemoteObjectInterface;
+use CRM_OSDI_ActionNetwork_TestUtils;
+use CRM_OSDI_Fixture_PersonMatching as PersonMatchFixture;
+use PHPUnit;
+
+/**
+ * @group headless
+ */
+class TaggingBasicTest extends PHPUnit\Framework\TestCase implements
+    \Civi\Test\HeadlessInterface,
+    \Civi\Test\TransactionalInterface {
+
+  private static \Civi\Osdi\ActionNetwork\Matcher\TaggingBasic $matcher;
+
+  private static \Civi\Osdi\ActionNetwork\RemoteSystem $remoteSystem;
+
+  public function setUpHeadless() {
+    return \Civi\Test::headless()->installMe(__DIR__)->apply();
+  }
+
+  public static function setUpBeforeClass(): void {
+    PersonMatchFixture::$personClass = \Civi\Osdi\ActionNetwork\Object\Person::class;
+
+    self::$remoteSystem = CRM_OSDI_ActionNetwork_TestUtils::createRemoteSystem();
+    self::$matcher = self::makeNewMatcher();
+  }
+
+  protected function setUp(): void {
+  }
+
+  protected function tearDown(): void {
+    parent::tearDown();
+  }
+
+  public static function tearDownAfterClass(): void {
+  }
+
+  private static function makeNewMatcher(): TaggingBasic {
+    $syncer = self::makeNewSyncer();
+    $matcher = $syncer->getMatcher();
+    /** @var \Civi\Osdi\ActionNetwork\Matcher\TaggingBasic $matcher */
+    return $matcher;
+  }
+
+  private static function makeNewSyncer(): \Civi\Osdi\ActionNetwork\SingleSyncer\TaggingBasic {
+    $remoteSystem = self::$remoteSystem;
+
+    $personSyncer = new \Civi\Osdi\ActionNetwork\SingleSyncer\Person\PersonBasic($remoteSystem);
+    $personSyncer->setMapper(new \Civi\Osdi\ActionNetwork\Mapper\PersonBasic($remoteSystem))
+      ->setMatcher(new \Civi\Osdi\ActionNetwork\Matcher\Person\UniqueEmailOrFirstLastEmail($remoteSystem));
+
+    $tagSyncer = new \Civi\Osdi\ActionNetwork\SingleSyncer\TagBasic($remoteSystem);
+    $tagSyncer->setMapper(new \Civi\Osdi\ActionNetwork\Mapper\TagBasic())
+      ->setMatcher(new \Civi\Osdi\ActionNetwork\Matcher\TagBasic($remoteSystem));
+
+    $taggingSyncer = new \Civi\Osdi\ActionNetwork\SingleSyncer\TaggingBasic($remoteSystem);
+    $taggingSyncer->setMapper(new \Civi\Osdi\ActionNetwork\Mapper\TaggingBasic($taggingSyncer))
+      ->setMatcher(new \Civi\Osdi\ActionNetwork\Matcher\TaggingBasic($taggingSyncer))
+      ->setPersonSyncer($personSyncer)
+      ->setTagSyncer($tagSyncer);
+
+    return $taggingSyncer;
+  }
+
+  private static function makePair($input): \Civi\Osdi\LocalRemotePair {
+    $pair = new \Civi\Osdi\LocalRemotePair();
+    $pair->setLocalClass(\Civi\Osdi\LocalObject\TaggingBasic::class);
+    $pair->setRemoteClass(\Civi\Osdi\ActionNetwork\Object\Tagging::class);
+    if (is_a($input, RemoteObjectInterface::class)) {
+      $pair->setRemoteObject($input)->setOrigin($pair::ORIGIN_REMOTE);
+    }
+    else {
+      $pair->setLocalObject($input)->setOrigin($pair::ORIGIN_LOCAL);
+    }
+    return $pair;
+  }
+
+  /**
+   * @return array{0: \Civi\Osdi\LocalObject\TaggingBasic, 1: \Civi\Osdi\ActionNetwork\Object\Tagging}
+   */
+  private function makeSameTaggingOnBothSides(): array {
+    $remotePerson = new \Civi\Osdi\ActionNetwork\Object\Person(self::$remoteSystem);
+    $remotePerson->emailAddress->set('taggingmatchertest@test.net');
+    $remotePerson->givenName->set('Test Tagging Matcher');
+    $remotePerson->save();
+
+    $localPerson = new \Civi\Osdi\LocalObject\PersonBasic();
+    $localPerson->emailEmail->set('taggingmatchertest@test.net');
+    $localPerson->firstName->set('Test Tagging Matcher');
+    $localPerson->save();
+
+    $remoteTag = new \Civi\Osdi\ActionNetwork\Object\Tag(self::$remoteSystem);
+    $remoteTag->name->set('Test Tagging Matcher');
+    $remoteTag->save();
+
+    $localTag = new \Civi\Osdi\LocalObject\TagBasic();
+    $localTag->name->set('Test Tagging Matcher');
+    $localTag->save();
+
+    $remoteTagging = new \Civi\Osdi\ActionNetwork\Object\Tagging(self::$remoteSystem);
+    $remoteTagging->setPerson($remotePerson);
+    $remoteTagging->setTag($remoteTag);
+    $remoteTagging->save();
+
+    $localTagging = new \Civi\Osdi\LocalObject\TaggingBasic();
+    $localTagging->setPerson($localPerson)->setTag($localTag);
+    $localTagging->save();
+
+    return [$localTagging, $remoteTagging];
+  }
+
+  public function testRemoteMatch_Success_NoSavedMatches() {
+    [$localTagging, $remoteTagging] = $this->makeSameTaggingOnBothSides();
+
+    self::assertNotNull($remoteTagging->getId());
+
+    $pair = $this->makePair($localTagging);
+    $matchFindResult = self::$matcher->tryToFindMatchFor($pair);
+
+    self::assertFalse($matchFindResult->isError());
+    self::assertTrue($matchFindResult->gotMatch());
+    self::assertEquals($remoteTagging->getId(), $matchFindResult->getMatch()->getId());
+  }
+
+  public function testLocalMatch_Success_NoSavedMatches() {
+    [$localTagging, $remoteTagging] = $this->makeSameTaggingOnBothSides();
+
+    self::assertNotNull($localTagging->getId());
+
+    $pair = $this->makePair($remoteTagging);
+    $matchFindResult = self::$matcher->tryToFindMatchFor($pair);
+
+    self::assertFalse($matchFindResult->isError());
+    self::assertTrue($matchFindResult->gotMatch());
+    self::assertEquals($localTagging->getId(), $matchFindResult->getMatch()->getId());
+  }
+
+  public function testLocalMatch_Success_WithSavedMatches() {
+    // could also be made part of preceding tests
+    self::markTestIncomplete('Todo');
+  }
+
+}
