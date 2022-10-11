5047a1552fd11b44e10d87345367d217321d7061
Noah Miller <nm@lemnisc.us>
2022-10-11 13:13:20 -0400

    Factory: WIP toward making it a more robust DI container/service locator
    see discussion at https://github.com/lemniscus/osdi-client/issues/12

--- Civi/Osdi/Factory.php
+++ Civi/Osdi/Factory.php
@@ -9,6 +9,7 @@ class Factory {
   public static array $registry = [
     'LocalObject' => [
       'Person' => LocalObject\PersonBasic::class,
+      'Tagging' => LocalObject\TaggingBasic::class,
     ],
     'OsdiObject' => [
       'osdi:people' => ActionNetwork\Object\Person::class,
@@ -30,7 +31,19 @@ class Factory {
       'Tag' => ActionNetwork\SingleSyncer\TagBasic::class,
       'Tagging' => ActionNetwork\SingleSyncer\TaggingBasic::class,
     ],
+    'BatchSyncer' => [
+      'Tagging' => ActionNetwork\BatchSyncer\TaggingBasic::class,
+    ],
+    'CrmEventResponder' => [
+      'Contact' => ActionNetwork\CrmEventResponder\PersonBasic::class,
+      'EntityTag' => ActionNetwork\CrmEventResponder\TaggingBasic::class,
+    ],
   ];
+
+  public static function canMake(string $category, string $key) {
+    $class = self::$registry[$category][$key] ?? NULL;
+    return !is_null($class);
+  }
 
   #[\ReturnTypeWillChange]
   public static function make(string $category, string $key, ...$constructorParams) {
@@ -43,6 +56,16 @@ class Factory {
 
   public static function register(string $category, string $key, string $class) {
     self::$registry[$category][$key] = $class;
+  }
+
+  public static function singleton(string $category, string $key, ...$constructorParams) {
+    $singletons = &\Civi::$statics['osdiClient.singletons'];
+    $singleton = $singletons[$category][$key] ?? NULL;
+    if (is_null($singleton)) {
+      $singleton = static::make($category, $key, ...$constructorParams);
+      $singletons[$category][$key] = $singleton;
+    }
+    return $singleton;
   }
 
 }
