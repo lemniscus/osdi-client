f61b9af76c2a66a18aeb9855f7705ddc8a9906dc
Noah Miller <nm@lemnisc.us>
2022-10-12 12:32:35 -0400

    OsdiPersonSyncState DAO entity: rename file from OsdiSyncState

---
+++ xml/schema/CRM/Osdi/PersonSyncState.entityType.php
@@ -0,0 +1,10 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+return [
+  [
+    'name' => 'OsdiPersonSyncState',
+    'class' => 'CRM_OSDI_DAO_PersonSyncState',
+    'table' => 'civicrm_osdi_person_sync_state',
+  ],
+];
