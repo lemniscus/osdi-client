6d6664a067a37baf58bf9107a93386964768b011
Noah Miller <nm@lemnisc.us>
2024-04-17 14:50:32 -0700

    TestUtils: try to not create a bunch of SyncProfiles

--- tests/phpunit/OsdiClient/ActionNetwork/TestUtils.php
+++ tests/phpunit/OsdiClient/ActionNetwork/TestUtils.php
@@ -27,13 +27,20 @@ class TestUtils {
   }
 
   public static function createSyncProfile(): array {
-    return \Civi\Api4\OsdiSyncProfile::create(FALSE)
-      ->addValue('is_default', TRUE)
-      ->addValue('label', 'SyncProfile created by ' . __CLASS__)
-      ->addValue('entry_point', 'https://actionnetwork.org/api/v2/')
-      ->addValue('api_token', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'apiToken'))
-      ->addValue('classes', [])
-      ->execute()->single();
+    \Civi\Api4\OsdiSyncProfile::update(FALSE)
+      ->addWhere('is_default', '=', TRUE)
+      ->addValue('is_default', FALSE)
+      ->execute();
+    return \Civi\Api4\OsdiSyncProfile::save(FALSE)
+      ->setMatch(['label'])
+      ->addRecord([
+        'is_default' => TRUE,
+        'label' => 'SyncProfile created by ' . __CLASS__,
+        'entry_point' => 'https://actionnetwork.org/api/v2/',
+        'api_token' => file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'apiToken'),
+        'classes' => [],
+      ])
+      ->execute()->first();
   }
 
 }
